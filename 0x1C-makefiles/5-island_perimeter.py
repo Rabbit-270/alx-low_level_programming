2df9610d800df9278a7e6a51499e36c56ba1120f
APINDA <apinda127@gmail.com>
2024-05-30 16:15:55 -0700

    Tested island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,11 +8,11 @@ def island_perimeter(grid):
     ''' calculates the island perimeter of grid. '''
     length = 0
     width = 0
-    for n in range(0, len(grid)+1):
+    for n in range(len(grid)):
         current_grid = grid[n]
         num_ones = 0
-        for o in range(0, len(current_grid) + 1):
-            if current_grid[o] == 0:
+        for o in range(len(current_grid)):
+            if current_grid[o] == 1:
                 num_ones += 1
         if num_ones != 0:
             length += 1
